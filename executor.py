13b6a299623e055f9b44dc2be344106025072e1b
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-10-27 16:28:29 +0100

    feat: add default args

--- executor.py
+++ executor.py
@@ -1,18 +1,49 @@
 from jina import Executor, DocumentArray, Document, requests
 from paddleocr import PaddleOCR
-
+from typing import Optional, Dict
+from jina.logging.logger import JinaLogger
 
 class PaddlepaddleOCR(Executor):
-    def __init__(self, *args, **kwargs):
-        super(PaddlepaddleOCR, self).__init__(*args, **kwargs)
-        self.model = PaddleOCR(use_angle_cls=True, lang='en', use_gpu=False)
+    """
+    An executor to extract text from images using paddlepaddleOCR
+    """
+    def __init__(
+        self,
+        paddleocr_args : Optional[Dict] = None,
+        **kwargs
+        ):
+        """
+        :param paddleocr_args: the arguments for `paddleocr` for extracting text. By default
+        `use_angle_cls=True`, `lang='en'`, `use_gpu=False` 
+        """
+        self._paddleocr_args = paddleocr_args or {}
+        self._paddleocr_args.setdefault('use_angle_cls', True) 
+        self._paddleocr_args.setdefault('lang', 'en')
+        self._paddleocr_args.setdefault('use_gpu', False)
+        super(PaddlepaddleOCR, self).__init__(**kwargs)
+        self.model = PaddleOCR(**self._paddleocr_args)
+        self.logger = JinaLogger(
+            getattr(self.metas, 'name', self.__class__.__name__)
+        ).logger
 
     @requests(on='/extract')
-    def extract(self, docs: DocumentArray, **kwargs):
+    def extract(self, docs: Optional[DocumentArray] = None, **kwargs):
+        """
+        Load the image from Document.uri extract text and bounding boxes. The result is stored
+        it into chunks
+        :param docs: the input Documents with image URI in the `uri` field
+        """
+        if docs is None:
+            return
         for doc in docs:
+            self.logger.info(f'received {doc.id}')
+
+            if doc.uri == '':
+                self.logger.error(f'No uri passed for the Document: {doc.id}')
+                continue
             for r in self.model.ocr(doc.uri, cls=True):
-                cord, (text, score) = r
+                coord, (text, score) = r
                 c = Document(text=text, weight=score)
-                c.tags['cordinates'] = cord
+                c.tags['coordinates'] = coord
                 doc.chunks.append(c)
                 print(f'{text}')
