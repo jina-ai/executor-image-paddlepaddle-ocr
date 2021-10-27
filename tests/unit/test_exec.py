bd2edc1fb5de1adeb7e98465945f01b3998c9df6
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-10-27 16:29:08 +0100

    test: test no documents no uris

--- tests/unit/test_exec.py
+++ tests/unit/test_exec.py
@@ -14,3 +14,17 @@ def test_exec():
     exec.extract(docs=docs)
     for d in docs:
         assert len(d.chunks) == 3
+
+def test_no_documents():
+    docs = DocumentArray()
+    exec = PaddlepaddleOCR()
+    exec.extract(docs=docs)
+    assert len(docs) == 0 
+
+
+def test_docs_no_uris():
+    docs = DocumentArray([Document()])
+    exec = PaddlepaddleOCR()
+    exec.extract(docs=docs)
+    assert len(docs) == 1
+    assert len(docs[0].chunks) == 0
