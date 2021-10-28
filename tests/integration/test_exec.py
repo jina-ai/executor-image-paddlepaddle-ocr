6e183dc743b0d1c575cf42052e4e29f74b710fd1
Aziz Belaweid <azizbellaweid@hotmail.com>
2021-10-28 10:03:38 +0100

    test: Added integration test

---
+++ tests/integration/test_exec.py
@@ -0,0 +1,24 @@
+from executor import PaddlepaddleOCR
+import pytest
+from pathlib import Path
+from jina import Document, DocumentArray, Flow
+
+
+@pytest.mark.parametrize("request_size", [1, 10, 50, 100])
+def test_integration(request_size: int):
+    doc1 = Document(uri=str(Path(__file__).parents[1] / 'toy-data' / 'test1.png'))
+    doc2 = Document(uri=str(Path(__file__).parents[1] / 'toy-data' / 'test2.png'))
+    docs = DocumentArray([doc1,doc2])
+
+    with Flow(return_results=True).add(uses=PaddlepaddleOCR) as flow:
+            resp = flow.post(
+                on="/index",
+                inputs=docs,
+                request_size=request_size,
+                return_results=True,
+            )
+    
+    assert sum(len(resp_batch.docs) for resp_batch in resp) == 2
+    for r in resp:
+        for doc in r.docs:
+            assert doc.text is not None    
